7287f3b1c8d5b5cc7f1412c8ca0e243a3543fe29
Mario Quesada <mario.quesada@doubledot.es>
2018-02-14 12:40:28 +0100

    cambios para dar soporte a base64

--- src/AppBundle/Entity/Archivo.php
+++ src/AppBundle/Entity/Archivo.php
@@ -4,6 +4,7 @@ namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 
+use Symfony\Component\HttpFoundation\File\File;
 
 
 /**
@@ -27,30 +28,65 @@ class Archivo extends Entity
     {      
           if($file!=null)
           {
-               $fecha=date("Y/m/d");
+               $nombre=$this->sanear_nombre($file->getClientOriginalName());
+               
+               $nombre=$this->calcular_nombre($nombre,$carpeta);
 
-               $ruta=$carpeta;
+               $file->move(".".$carpeta,$nombre);
 
-               $format=$file->getClientOriginalExtension();
-
-               $name=str_replace(".".$format, "",$file->getClientOriginalName());
-
-               $name=str_replace(" ","_", $name);
-
-               $files=glob(".".$ruta.$name."*.".$format);
-
-
-               $total=count($files);
-               if($total)
-                $name=$name."_".$total.".".$format;
-               else
-                $name=$name.".".$format;
-
-               $file->move(".".$ruta,$name);
-
-               return $ruta.$name;
+               return $carpeta.$nombre;
           }
        
+    }
+
+    protected function subir_base64($carpeta,$base64_string)
+    {
+      $temp_file = tempnam(sys_get_temp_dir(), 'aj');
+      
+
+      $base64_string=explode("+-+", $base64_string);
+      
+      $nombre= $this->sanear_nombre($base64_string[0]);
+      
+
+      $nombre= $this->calcular_nombre($nombre,$carpeta);
+      
+      $base64_string=explode(",", $base64_string[1]);
+
+      
+
+      $base64_string=base64_decode($base64_string[1]);
+
+      file_put_contents($temp_file,$base64_string);
+
+      $file=new File($temp_file);
+
+      $file->move(".".$carpeta,$nombre);
+
+      return $carpeta.$nombre;
+
+    }
+
+    private function calcular_nombre($nombre,$carpeta)
+    {
+      $format=substr($nombre,strrpos($nombre, ".")+1);
+      $nombre=substr($nombre,0,strrpos($nombre, "."));
+      
+      $files=glob(".".$carpeta.$nombre."*.".$format);
+
+
+      $total=count($files);
+      if($total)
+        $nombre=$nombre."_".$total.".".$format;
+      else
+        $nombre=$nombre.".".$format;
+
+      return $nombre;
+    }
+
+    private function sanear_nombre($nombre)
+    {
+      return str_replace(" ","_", $nombre);
     }
 
 
