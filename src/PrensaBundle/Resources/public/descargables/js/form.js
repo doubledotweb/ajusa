ad5381aed1a4ae839f0d9fd77bbda759324b00e7
Mario Quesada <mario.quesada@doubledot.es>
2018-02-14 12:42:43 +0100

    recorte y filtrado del tamaño de las imagenes antes de enviar

--- src/PrensaBundle/Resources/public/descargables/js/form.js
+++ src/PrensaBundle/Resources/public/descargables/js/form.js
@@ -8,22 +8,49 @@ $(document).ready(function(e)
 	$('input[name="'+name+'[categoria]"]').on("change",cambio_categoria);
 
 
-	$('input[name="'+name+'[archivo_aux]"]').on("change",gestion_archivo);	
+	$('input[name="'+name+'[archivo_aux]"],#fake_button_file').on("change",gestion_archivo);	
 
 	$("#"+name+"_delete").on("click",borrar);
 
 	$(".js-borrar-imagen").on("click",borrar_imagen);
 
 
-	$("form").submit(function(event) {
-		if($('input[name="'+name+'[archivo_aux]"]')[0].files[0].size/1024/1024>2)
-		{
-			event.preventDefault();
-			mostrar_popup("El archivo supera el tamaño máximo permitido","ok");
-			$("#popup #box_options").on("click","#decline",cancelar);
-		}
-	});
+	$("form").submit(enviar_form);
 	
+	text=$("#croppie").croppie({		
+		"viewport": 
+		{
+        	width: 200,
+        	height: 200
+        }
+    })
+	
+
+    $("#croppie-save").on("click",function(e)
+    {
+    	e.preventDefault();
+
+	    	text.croppie('result', {
+			type: 'base64',
+			
+			// size: { width: 300, height: 300 },
+	    	format: 'png'
+	    }).then(function (canvas) {
+				
+				$(".file-field img").attr("src",canvas);
+				$('input[name="'+name+'[imagen]"]').val(canvas);
+				$("#popup-croppie").hide();
+				$(".file-field").find(".js-imagen-preview").show();
+				$(".file-field").find(".js-imagen-input").hide();
+
+				
+		});
+
+    })
+
+
+	text.width(300);
+	text.height(300);
 	
 });
 
@@ -74,11 +101,17 @@ function cambio_categoria(e)
 
 	if(categoria=="clipping-de-prensa")
 	{
-		$('input[name="'+name+'[archivo_aux]"]').attr("accept",".pdf,.doc,.zip");
+		$(".js-archivo").show();
+		$(".js-archivo input[name]").attr('requried',1);
+		$("#fake_button_file").removeAttr("required");
+		$(".js-imagen").hide();
 	}
 	else
 	{
-		$('input[name="'+name+'[archivo_aux]"]').attr("accept","image/*");
+		$(".js-archivo").hide();
+		$(".js-imagen").show();
+		$("#fake_button_file").attr('requried',1);
+		$(".js-archivo input[name]").removeAttr("required");
 	}
 }
 
@@ -95,11 +128,15 @@ function gestion_archivo(e)
 
 		var aux=fake.split("\\");
 
-		$(this).parents(".js-archivo-input").find("input.file-path").val(aux[aux.length-1]);
+		$(this).parents(".js-imagen-input").find("input.file-path").val(aux[aux.length-1]);
 	break;
 
 	case "imagen":
 	case "logotipo":
+
+
+		
+		
 	
 		input = $(this);
 
@@ -107,16 +144,69 @@ function gestion_archivo(e)
 		reader = new FileReader();
 		reader.onload = function(event) {	    		
 
-			input.parents(".file-field").find(".js-imagen-preview img").prop("src",event.target.result);
-			input.parents(".file-field").find(".js-imagen-preview").show();
-			input.parents(".file-field").find(".js-imagen-input").hide();
+			//input.parents(".file-field").find(".js-imagen-preview img").prop("src",event.target.result);
+			//input.parents(".file-field").find(".js-imagen-preview").show();
+			//input.parents(".file-field").find(".js-imagen-input").hide();
+			text.croppie("bind",{"url":event.target.result});
+			$("#popup-croppie").show();
+			//console.log(event);
 		};
 
 		if(input.prop("files") && input.prop("files")[0])
-			reader.readAsDataURL(input.prop("files")[0]); 
+			reader.readAsDataURL(input.prop("files")[0]);
 
 	break;
 	}
 
 	
+}
+
+function enviar_form(event) 
+{
+	var categoria=$('input[name="'+name+'[categoria]"]:checked').val();
+
+	var archivo_size=0;
+
+	switch(categoria)
+	{
+
+		case "clipping-de-prensa":
+			if($('input[name="'+name+'[archivo_aux]"]')[0].files.length>0)
+				archivo_size=$('input[name="'+name+'[archivo_aux]"]')[0].files[0].size/1024/1024;
+		break;
+
+		case "logotipo":
+		case "imagen":
+			var imagen=$('input[name="'+name+'[imagen]"]').val();
+
+			var head = imagen.substr(0,imagen.indexOf(","));
+			archivo_size = Math.round((imagen.length - head.length)*3/4)/1024/1024 ;
+
+		break
+	
+	}	
+
+	
+
+	if( archivo_size>2)
+	{
+		event.preventDefault();
+		mostrar_popup("El archivo supera el tamaño máximo permitido","ok");
+		$("#popup #box_options").on("click","#decline",cancelar);
+
+		return false;
+	}
+	
+
+
+	if(categoria=="imagen"||categoria=="logotipo")
+	{
+		if($("#fake_button_file")[0].files.length)
+		{
+
+			var nombre_imagen=$("#fake_button_file")[0].files[0].name;
+
+			$('input[name="'+name+'[imagen]"]').val(nombre_imagen+"+-+"+$('input[name="'+name+'[imagen]"]').val());
+		}
+	}
 }
