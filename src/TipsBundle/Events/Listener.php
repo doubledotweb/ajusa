da5c5cf59dd4b6b511385c1a9af0757ea35ae484
Mario Quesada <mario.quesada@doubledot.es>
2018-02-28 12:06:28 +0100

    [TipsBundle]: gestor categorias

---
+++ src/TipsBundle/Events/Listener.php
@@ -0,0 +1,55 @@
+<?php
+// src/Acme/SearchBundle/EventListener/SearchIndexerSubscriber.php
+namespace TipsBundle\Events;
+
+use Doctrine\Common\EventSubscriber;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+// for Doctrine 2.4: Doctrine\Common\Persistence\Event\LifecycleEventArgs;
+use TipsBundle\Entity\Categoria;
+
+class Listener implements EventSubscriber
+{
+
+    private $normalizator;
+
+    public function __construct($nor)
+    {
+        $this->normalizator=$nor;
+    }
+
+    public function getSubscribedEvents()
+    {
+        return array(
+            'prePersist',
+            'preUpdate',
+        );
+    }
+
+    public function preUpdate(LifecycleEventArgs $args)
+    {
+        $this->index($args);
+    }
+
+    public function prePersist(LifecycleEventArgs $args)
+    {
+        $this->index($args);
+    }
+
+    public function index(LifecycleEventArgs $args)
+    {
+        $entity = $args->getEntity();
+        $entityManager = $args->getEntityManager();
+
+        // perhaps you only want to act on some "Product" entity
+        if ($entity instanceof Categoria) 
+        {
+            
+            $slug=$this->normalizator->calculate_slug($entity->getTitulo());
+
+            $entity->setSlug($slug);
+        }
+    }
+}
+
+
+?>
