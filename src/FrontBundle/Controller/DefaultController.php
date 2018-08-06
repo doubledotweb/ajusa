9acfe2777f5caff5ae4abe3c56c5c2ada8abb4a0
Millan <millanhermana@doubledot.com>
2018-08-06 11:45:26 +0200

    Vuelvo a la plantilla original

--- src/FrontBundle/Controller/DefaultController.php
+++ src/FrontBundle/Controller/DefaultController.php
@@ -600,8 +600,8 @@ class DefaultController extends BaseController
       ->setTo("millan.hermana@doubledot.es")
       ->setBody(                  
           $this->renderView(
-            "app/Resources/views/email.html.twig",
-              //"base.html.twig",
+              // app/Resources/views/Emails/registration.html.twig
+              "base.html.twig",
               array("datos"=>$mensaje)
           ),
           'text/html'
